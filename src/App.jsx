abadf7f1cbf6987c4efa74ec69bfa3258f196669
Imran4191 <imran@bwipholdings.com>
2024-12-28 13:32:14 +0600

    learn useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
-import { useReducer, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import TaskList from "./components/TaskList";
-import AddTask from "./components/AddTask";
-import TaskProvider from "./contexts/TaskContext";
+import { use } from "react";
 
-
-let nextId = 4;
 function App() {
+  const [count, setCount] = useState(0);
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts?_limit=5")
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
+  }
+  , []);
   return (
     <div className="App">
-      <TaskProvider>
-        <AddTask />
-        <TaskList 
-        />
-      </TaskProvider>
+      <div className="count">
+        <h2>Counter is : {count}</h2>
+        <button onClick={() => setCount(count+1)}>Increment</button>
+      </div>
+      <div className="posts">
+        <h2>All Posts</h2>
+        <ul
+          style={{
+            listStyle: "none",
+            padding: 0,
+            margin: 0,
+          }}>
+          {posts.map((post) => (
+            <li key={post.id} className="post">
+              <h3>{post.title}</h3>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
